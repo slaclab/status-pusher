67530fc29d64ad93aacc828157cf80d4837c5919
Micha Okun <mokun@slac.stanford.edu>
2025-02-06 11:36:30 -0800

    correct timestamp format req'd by frontend

--- test/util/generate_fake_test_data.py
+++ test/util/generate_fake_test_data.py
@@ -7,8 +7,8 @@ import datetime as dt
 import io
 
 DAYS = 90
-START_DATE = dt.datetime.now() - dt.timedelta(days=DAYS)
-END_DATE = dt.datetime.now()
+START_DATE = dt.datetime.utcnow() - dt.timedelta(days=DAYS)
+END_DATE = dt.datetime.utcnow()
 
 FAKE_VALUE = 3.1415926535897
 
@@ -25,7 +25,7 @@ def gen_fake_log_data(
         start_date + dt.timedelta(days=d) for d in range((end_date - start_date).days)
     ]
     # note we want the spaces preceding the values to match Fettle's default format
-    data = [[date.isoformat(), " success", f" {fake_value}"] for date in dates]
+    data = [[date.strftime('%Y-%m-%dT%H:%M:%SZ'), " success", f" {fake_value}"] for date in dates]
     output = io.StringIO()
     writer = csv.writer(output, delimiter=",")
     writer.writerows(data)
